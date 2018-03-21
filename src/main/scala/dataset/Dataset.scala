1063c18374418ae9f137e865dea662a02315eaf4
gr3g <gregoire.clement@epfl.ch>
2018-03-21 11:10:00 +0100

    dataset object improved

--- src/main/scala/dataset/Dataset.scala
+++ src/main/scala/dataset/Dataset.scala
@@ -3,20 +3,62 @@ package dataset
 import scala.io.{BufferedSource, Source}
 
 object Dataset {
-  type Data = Any
+
+  type Data = Map[Int, Document]
+  type Document = Map[Int, Float]
+  type Label = String
+
+  private val dataPath = "data/"
+
+  val filePaths: List[String] = (0 until 4).map(i => dataPath + filename(i)).toList
+
+  lazy val numLinesPerFile: List[Int] = filePaths.map(Source.fromFile).map(_.getLines().size)
+
+  lazy val startingIndexPerFile: List[Int] = {
+    val numFiles = numLinesPerFile.size
+    0 :: (1 until numFiles).map(i => numLinesPerFile.dropRight(numFiles - i).sum).toList
+  }
+
+  lazy val docIndexToLineIndex: Map[Int, Int] = {
+    filePaths.map(Source.fromFile).flatMap(_.getLines().map(_.split(" ").head.toInt)).zipWithIndex.toMap
+  }
+
+  lazy val lineIndexToDocIndex: Map[Int, Int] = {
+    docIndexToLineIndex.map{case(k, v) => v -> k}
+  }
 
   def load: Data = {
-    val path = "data/"
-    def filename(i: Int) = s"lyrl2004_vectors_test_pt$i.dat"
-    val filePaths = (0 until 4).map(i => path + filename(i))
-    var n = 0
-    val data: BufferedSource = Source.fromFile(filePaths.head)
-    for (line <- data.getLines if n < 100) {
-      val cols = line.split(" ").map(_.trim)
-      // do whatever you want with the columns here
-      n += 1
-      println(s"${cols(0)}|${cols(1)}|${cols(2)}|${cols(3)}")
-    }
-    data.close
+
+    println(numLinesPerFile)
+
+    val source: BufferedSource = Source.fromFile(filePaths.head)
+
+    val data = source.getLines().map { line =>
+
+      val line_splitted = line.split(" ").map(_.trim).filter(!_.isEmpty).toList
+      val did: Int = line.head.toInt
+      did -> toDocument(line_splitted)
+
+    }.toMap
+
+    source.close
+    data
   }
+
+  private def toDocument(line: List[String]): Document = {
+    line.tail.map(e => {
+      val pair: List[String] = e.split(":").map(_.trim).toList
+      pair.head.toInt -> pair.tail.head.toFloat
+    }).toMap
+  }
+
+  def get(index: Int, isDocumentIndex: Boolean = false): Document = {
+    ???
+  }
+
+  def getLabel(index: Int, isDocumentIndex: Boolean = false): Label = {
+    ???
+  }
+
+  private def filename(i: Int) = s"lyrl2004_vectors_test_pt$i.dat"
 }
--- src/main/scala/testing/Main.scala
+++ src/main/scala/testing/Main.scala
@@ -1,8 +1,17 @@
 package testing
 
 import dataset.Dataset
+import dataset.Dataset.numLinesPerFile
 
 object Main extends App {
 
-  Dataset.load
+  //print(Int.MaxValue)
+  println(
+    Dataset.numLinesPerFile,
+    Dataset.startingIndexPerFile,
+  )
+
+  val map = Dataset.lineIndexToDocIndex
+
+  println(map.values.toList.sorted.take(100))
 }
