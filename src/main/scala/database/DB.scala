d8359b622f4aa3091e905b2c8bd533897804a02a
maxime-delisle <maxime.delisle@epfl.ch>
2018-03-24 17:17:11 +0100

    added function to load part of lines by part of lines into database

--- src/main/scala/database/DB.scala
+++ src/main/scala/database/DB.scala
@@ -1,43 +1,65 @@
 package database
 
-import slick.dbio.Effect.{ Read, Schema, Write }
+import slick.dbio.Effect.{Read, Schema, Write}
+
+import util.Types.{Counts, SparseVector}
+
 
 import scala.concurrent._
 import scala.concurrent.duration._
 import slick.jdbc.PostgresProfile.api._
-import slick.lifted.{ ProvenShape, TableQuery }
-import slick.sql.{ FixedSqlAction, FixedSqlStreamingAction }
+import slick.lifted.TableQuery
+import slick.sql.{FixedSqlAction, FixedSqlStreamingAction}
+
+import scala.io.Source
 
 object DB extends App {
+
+
+  private def parseLine(line: String): (Int, SparseVector) = {
+    val lineSplitted = line.split(" ").map(_.trim).filterNot(_.isEmpty).toList
+    val did: Int = lineSplitted.head.toInt
+    did -> pairsToDocument(lineSplitted.tail)
+  }
+
+  private def pairsToDocument(lineSplitted: List[String]): SparseVector = {
+    lineSplitted.map(e => {
+      val pair: List[String] = e.split(":").map(_.trim).toList
+      pair.head.toInt -> pair.tail.head.toDouble
+    }).toMap
+  }
 
   lazy val featureTable = TableQuery[FeatureTable]
 
   val createTableAction: FixedSqlAction[Unit, NoStream, Schema] = featureTable.schema.create
 
-  val insertAlbumsAction: FixedSqlAction[Option[Int], NoStream, Write] = featureTable ++= Seq(
-    FeatureSchema(0, 0, 33.33),
-    FeatureSchema(1, 0, 34.33),
-    FeatureSchema(2, 0, 35.33),
-    FeatureSchema(1, 2, 36.33),
-    FeatureSchema(1, 10, 37.33),
-    FeatureSchema(2, 10, 38.33),
-  )
+  def insertAlbumsAction(lines:Seq[String]): FixedSqlAction[Option[Int], NoStream, Write] = featureTable ++=
+    lines.map(line => parseLine(line)).flatMap(x => x._2.map(y => FeatureSchema(x._1, y._1, y._2)))
+
+  def insertFile(filename:String): Unit = {
+    Source.fromFile(filename).getLines().sliding(1000, 1000).foreach(lines => exec(insertAlbumsAction(lines)))
+  }
 
   val selectAlbumsActions: FixedSqlStreamingAction[Seq[FeatureSchema], FeatureSchema, Read] = featureTable.result
 
   val query = for {
-    f <- featureTable if f.did === 1l
+    f <- featureTable if f.did === 1523l
   } yield f.tid -> f.weight
 
   private val db = Database.forConfig("scalaxdb")
 
   private def exec[T](action: DBIO[T]): T =
-    Await.result(db.run(action), 2.seconds)
+    Await.result(db.run(action), 1200.seconds)
 
   exec(createTableAction)
-  exec(insertAlbumsAction)
-  exec(selectAlbumsActions).foreach(println)
+  println("Loading start")
+  val startTimeL = System.currentTimeMillis()
+  insertFile(s"data/lyrl2004_vectors_test_pt0.dat")
+  println("Final loading ended after "+(System.currentTimeMillis() - startTimeL)/1000+" seconds")
+  //exec(selectAlbumsActions)
+  val startTimeQ = System.currentTimeMillis()
   println(exec(query.result).toMap)
+  println("Time to answer 1 query: "+(System.currentTimeMillis() - startTimeQ)+" milli-seconds")
 
 
 }
