db0f9b22fbf963a66bff78f941661f3b41de4156
maxime-delisle <maxime.delisle@epfl.ch>
2018-05-21 15:59:00 +0200

    added start of yaml config files

--- start_node.sh
+++ start_node.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
-sbt "run $mode $type"
+
+#This will be set elsewhere later on
+interval=500
+#This will be set elsewhere later on
+port=80
+
+
+n=`echo $1 | awk -F- '{print $NF}'`
+last_number=`echo $2 | cut -d . -f 4`
+difference=$((last_number-n))
+first_ip=`echo $2 | cut -d"." -f1-3`.$difference
+
+if [$n -eq 0 ];then
+  if [$MODE -eq "async" ];then
+    sbt "run async ${port} ${interval}"
+  else
+    sbt "run sync coord ${port}"
+  fi
+else
+  if [$MODE -eq "async" ];then
+    sbt "run async ${port} ${first_ip} ${port} ${interval}"
+  else
+    sbt "run sync worker ${first_ip} ${port}"
+  fi
+fi
