bdbb922d881f3bba722c0bd19961a62c619ff65b
maxime-delisle <maxime.delisle@epfl.ch>
2018-05-23 10:38:12 +0200

    added last modifs

--- delete_pods.sh
+++ delete_pods.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-kubectl delete service hogwild
-kubectl delete statefulset hogwild --cascade=false
+kubectl delete service dns-service
+kubectl delete service statefulset-service
+kubectl delete deployment hogwild-master
+kubectl delete statefulset hogwild-slave --cascade=false
 kubectl delete pods --all
