868b3c914afd8f40b0de956e7800cb6774e0ae60
maxime-delisle <maxime.delisle@epfl.ch>
2018-05-23 14:05:35 +0200

    removed dns

--- target/docker/stage/opt/docker/bin/start_node.sh
+++ target/docker/stage/opt/docker/bin/start_node.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-#n=`echo $MY_POD_NAME | awk -F- '{print $NF}'`
-#last_number=`echo $MY_POD_IP | cut -d . -f 4`
-#difference=$((last_number-n))
-#first_ip=`echo $MY_POD_IP | cut -d"." -f1-3`.$difference
+n=`echo $MY_POD_NAME | awk -F- '{print $NF}'`
+last_number=`echo $MY_POD_IP | cut -d . -f 4`
+difference=$((last_number-n))
+first_ip=`echo $MY_POD_IP | cut -d"." -f1-3`.$difference
 
-ip_port=dns-service:${PORT_TO_OPEN}
+ip_port=hogwild-pod-0:${PORT_TO_OPEN}
 
-if [ "$ROLE" == "master" ];then
+if [ "$n" == "0" ];then
   ip_port=""
 fi
 
