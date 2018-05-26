eb76f9f4a893a152c63070bb518387156640e965
gr3g <gregoire.clement@epfl.ch>
2018-05-26 20:38:21 +0200

    removed useless files

--- src/main/scala/Testing.scala
+++
@@ -1,9 +0,0 @@
-import dataset.Dataset
-
-object Testing extends App {
-
-  val d = Dataset("data/")
-  println(d.tids.toSet diff d.inverseTidCountsVector.keys)
-  println(d.inverseTidCountsVector.keys diff d.tids.toSet)
-
-}
--- target/docker/stage/opt/docker/bin/testing.bat
+++
@@ -1,5 +0,0 @@
-@REM Forwarder script
-
-@echo off
-
-%0\..\hogwild -main Testing %*
