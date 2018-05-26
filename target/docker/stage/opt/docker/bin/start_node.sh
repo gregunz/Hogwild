492961df434e27e9d9a54ef9a1bd5f5f3495a367
maxime-delisle <maxime.delisle@epfl.ch>
2018-05-26 22:30:07 +0200

    removed early stopping option for other nodes than the first one

--- target/docker/stage/opt/docker/bin/start_node.sh
+++ target/docker/stage/opt/docker/bin/start_node.sh
@@ -2,11 +2,12 @@
 
 n=`echo $MY_POD_NAME | awk -F- '{print $NF}'`
 first_ip="hogwild-0"
-
+early_stop=""
 ip_port=hogwild-pod-0.statefulset-service:${PORT_TO_OPEN}
 
 if [ "$n" == "0" ];then
   ip_port=hogwild-pod-1.statefulset-service:${PORT_TO_OPEN}
+  early_stop=${EARLY_STOPPING}
 fi
 
 sh hogwild \
@@ -16,6 +17,6 @@ sh hogwild \
     ip:port=${ip_port} \
     data-path=${DATASET_PATH} \
     lambda=${LAMBDA} \
-    early-stopping=${EARLY_STOPPING} \
+    early-stopping=early_stop \
     min-loss=${MIN_LOSS} \
     step-size=${STEP_SIZE}
