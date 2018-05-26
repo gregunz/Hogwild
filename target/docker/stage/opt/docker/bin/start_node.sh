94abf05532fedb57105b2fa0c35864f71bf186ac
maxime-delisle <maxime.delisle@epfl.ch>
2018-05-26 22:34:11 +0200

    same for min-loss

--- target/docker/stage/opt/docker/bin/start_node.sh
+++ target/docker/stage/opt/docker/bin/start_node.sh
@@ -3,11 +3,13 @@
 n=`echo $MY_POD_NAME | awk -F- '{print $NF}'`
 first_ip="hogwild-0"
 early_stop=""
+min-loss=""
 ip_port=hogwild-pod-0.statefulset-service:${PORT_TO_OPEN}
 
 if [ "$n" == "0" ];then
   ip_port=hogwild-pod-1.statefulset-service:${PORT_TO_OPEN}
   early_stop=${EARLY_STOPPING}
+  loss=${EARLY_STOPPING}
 fi
 
 sh hogwild \
@@ -17,6 +19,6 @@ sh hogwild \
     ip:port=${ip_port} \
     data-path=${DATASET_PATH} \
     lambda=${LAMBDA} \
+    step-size=${STEP_SIZE} \
     early-stopping=early_stop \
-    min-loss=${MIN_LOSS} \
-    step-size=${STEP_SIZE}
+    min-loss=loss
