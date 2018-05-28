02dc9d8607d688cabf565548ea8570cb5de7f816
gr3g <gregoire.clement@epfl.ch>
2018-05-28 22:06:22 +0200

    log added

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+MODE=$1
+REPLICAS=$2
+
 cat kubernetes/hogwild.yaml | \
-    sed "s/__REPLICAS__/$2/g" | \
-    sed "s/__MODE__/$1}/g" | \
+    sed "s/__REPLICAS__/"${REPLICAS}"/g" | \
+    sed "s/__MODE__/"${MODE}"/g" | \
     kubectl create -f -
